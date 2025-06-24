8c296fe28cc20424951ff644da173eb82870f09c
hodaridata <maciej.j.chrzanowski@gmail.com>
2025-06-24 17:39:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+#Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -18,8 +18,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingedients', 
                                   my_dataframe,
@@ -27,6 +27,9 @@ ingredients_list = st.multiselect('Choose up to 5 ingedients',
 
 if ingredients_list:
     ingredients_string = ''
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+  
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
